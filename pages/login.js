ca311b2f50a5a73195de38b63da1aedafabb9203
Moses Fawole <mosesfawole2001@gmail.com>
2022-07-28 08:00:05 +0100

    create components

---
+++ pages/login.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Home.module.css";
+
+const Login = () => {
+  return (
+    <main className={styles.main}>
+      <h1>Login</h1>
+    </main>
+  );
+};
+
+export default Login;
---
+++ pages/register.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Home.module.css";
+
+const Register = () => {
+  return (
+    <main className={styles.main}>
+      <h1>Register</h1>
+    </main>
+  );
+};
+
+export default Register;
